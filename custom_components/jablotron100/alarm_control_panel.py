5d31b98c0fcd8b3c8597e2cd1d03611d2736a2c2
Jaroslav Hanslík <kukulich@kukulich.cz>
2024-05-29 23:30:56 +0200

    Use new native attribute for alarm control panel and some other cleanup

--- custom_components/jablotron100/alarm_control_panel.py
+++ custom_components/jablotron100/alarm_control_panel.py
@@ -42,6 +42,8 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, asyn
 class JablotronAlarmControlPanelEntity(JablotronEntity, AlarmControlPanelEntity):
 	_control: JablotronAlarmControlPanel
 	_changed_by: str | None = None
+	_code_required_for_disarm: bool = False
+	_partially_arming_mode: PartiallyArmingMode
 
 	_attr_name = None
 
@@ -55,6 +57,10 @@ class JablotronAlarmControlPanelEntity(JablotronEntity, AlarmControlPanelEntity)
 	def _update_attributes(self) -> None:
 		super()._update_attributes()
 
+		self._partially_arming_mode = self._jablotron.partially_arming_mode()
+		self._code_required_for_disarm = self._jablotron.is_code_required_for_disarm()
+
+		self._attr_code_arm_required = self._jablotron.is_code_required_for_arm()
 		self._attr_supported_features = self._detect_supported_features()
 		self._attr_state = self._get_state()
 		self._attr_changed_by = self._changed_by
@@ -66,7 +72,7 @@ class JablotronAlarmControlPanelEntity(JablotronEntity, AlarmControlPanelEntity)
 
 		code = JablotronAlarmControlPanelEntity._clean_code(code)
 
-		if code is None and self._jablotron.is_code_required_for_disarm():
+		if code is None and self._code_required_for_disarm:
 			return
 
 		self._jablotron.modify_alarm_control_panel_section_state(self._control.section, STATE_ALARM_DISARMED, code)
@@ -77,7 +83,7 @@ class JablotronAlarmControlPanelEntity(JablotronEntity, AlarmControlPanelEntity)
 
 		code = JablotronAlarmControlPanelEntity._clean_code(code)
 
-		if code is None and self._jablotron.is_code_required_for_arm():
+		if code is None and self._attr_code_arm_required:
 			return
 
 		self._jablotron.modify_alarm_control_panel_section_state(self._control.section, STATE_ALARM_ARMED_AWAY, code)
@@ -100,27 +106,25 @@ class JablotronAlarmControlPanelEntity(JablotronEntity, AlarmControlPanelEntity)
 
 		code = JablotronAlarmControlPanelEntity._clean_code(code)
 
-		if code is None and self._jablotron.is_code_required_for_arm():
+		if code is None and self._attr_code_arm_required:
 			return
 
 		self._jablotron.modify_alarm_control_panel_section_state(self._control.section, state, code)
 
 	def _detect_supported_features(self) -> AlarmControlPanelEntityFeature:
-		partially_arming_mode = self._jablotron.partially_arming_mode()
-
-		if partially_arming_mode == PartiallyArmingMode.NOT_SUPPORTED:
+		if self._partially_arming_mode == PartiallyArmingMode.NOT_SUPPORTED:
 			return AlarmControlPanelEntityFeature.ARM_AWAY
 
-		if partially_arming_mode == PartiallyArmingMode.HOME_MODE:
+		if self._partially_arming_mode == PartiallyArmingMode.HOME_MODE:
 			return AlarmControlPanelEntityFeature.ARM_AWAY | AlarmControlPanelEntityFeature.ARM_HOME
 
 		return AlarmControlPanelEntityFeature.ARM_AWAY | AlarmControlPanelEntityFeature.ARM_NIGHT
 
 	def _detect_code_format(self) -> CodeFormat | None:
 		if self._get_state() == STATE_ALARM_DISARMED:
-			code_required = self._jablotron.is_code_required_for_arm()
+			code_required = self._attr_code_arm_required
 		else:
-			code_required = self._jablotron.is_code_required_for_disarm()
+			code_required = self._code_required_for_disarm
 
 		if not code_required:
 			return None
